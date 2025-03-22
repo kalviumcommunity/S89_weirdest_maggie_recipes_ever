57348e45ebe86df61750b25cac0644de7cfc5de8
madhu-software <inaparthi.madhukiran.s89@kalvium.community>
2025-03-22 10:48:46 +0530

    done

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = 5000;
-
+require('dotenv').config();
 app.get('/ping', (req, res) => {
   res.send('pong');
 });
@@ -10,10 +10,16 @@ app.get('/ping', (req, res) => {
 app.listen(PORT, async () => {
   try {
     
-    await mongoose.connect('mongodb+srv://madhukiraninaparthi2001:madhukiraninaparthi2006@cluster0.ewsp8.mongodb.net/');
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+  });
     console.log('Connected to MongoDB database');
   } catch (err) {
-    console.log(err);
+    console.log("failed to connect",err);
   }
   console.log(`Server is running on port ${PORT}`);
+});
+app.get('/',(req, res) => {
+  res.status(201).send('connected to MongoDB server successfully!');
 });
